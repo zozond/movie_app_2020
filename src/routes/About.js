47f9b114b68d32f37f8abd565393cbdd87763736
DESKTOP-RCGL6QP\sjh <zozond@naver.com>
2020-11-22 23:48:28 +0900

    finished

---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function About(props) {
+    console.log(props);
+    return <span> About this page: I built it because I love movies. </span>
+}
+
+export default About;
